2ce4fd8cc20dc5364096c605e87d6ad8e5f0ce58
Jemal-tech <jemalahmed.tech@gmail.com>
2026-04-17 14:35:58 +0300

    Add assembly code to compute sum of squares
    Signed-off-by: Jemal-tech <jemalahmed.tech@gmail.com>

---
+++ labs/lab-05/tasks/sum-squared/support/sum_n_squared.asm
@@ -0,0 +1,29 @@
+; SPDX-License-Identifier: BSD-3-Clause
+
+%include "printf64.asm"
+
+section .data
+    num dq 100000
+
+section .text
+extern printf
+global main
+main:
+    push rbp
+    mov rbp, rsp
+
+    mov rcx, [num]     ; Use rcx as counter for computing the sum.
+    xor rbx, rbx       ; Use rbx to store the sum. Start from 0.
+
+add_to_sum:
+    mov rax, rcx
+    mul rax
+    
+    add rbx, rax 
+    loop add_to_sum    ; Decrement rcx. If not zero, add it to sum.
+
+    mov rcx, [num]
+    PRINTF64 `Sum(%lu): %lu\n\x0`, rcx, rbx
+
+    leave
+    ret
